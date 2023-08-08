19f3a0366a1a822b16c6530dcb7a481270c289ce
Joel <k.joel2@hotmail.com>
2023-08-07 21:44:29 -0500

    cambio de estructura

---
+++ 30-Days-Of-Python/day3_operators.py
@@ -0,0 +1,7 @@
+#day3 operators
+#https://github.com/Asabeneh/30-Days-Of-Python/blob/master/03_Day_Operators/03_operators.md
+
+radius = 10
+area_of_circle = 3.14 * radius ** 2  
+
+print(len('mango') == len('avocado'))
